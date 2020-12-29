9fb9e478b31187a6e3485a092b622c962664a179
henry brooks <brook057@cougars.csusm.edu>
2020-12-28 22:23:23 -0800

    Second part of tutorial

--- src/agents_of_swing/core.clj
+++ src/agents_of_swing/core.clj
@@ -1,7 +1,8 @@
 (ns agents-of-swing.core
   (:gen-class))
 
-(import '(javax.swing JPanel JFrame JButton JTextField JLabel Timer SwingUtilities))
+(import '(javax.swing JPanel JFrame JButton JTextField JLabel Timer SwingUtilities)
+        '(java.awt GridBagLayout Insets))
 
 (defn new-flipper []
   (agent {:total 0
@@ -43,7 +44,33 @@
       (proxy [java.awt.event.ActionListener] []
         (actionPerformed [~event] ~@body))))
 
-(defn flipper-app []
+(defmacro set-grid! [constraints field value]
+  `(set! (. ~constraints ~(symbol (name field)))
+         ~(if (keyword? value)
+            `(. java.awt.GridBagConstraints
+                ~(symbol (name value)))
+            value)))
+
+(defmacro grid-bag-layout [container & body]
+  (let [c (gensym "c")
+        cntr (gensym "cntr")]
+    `(let [~c (new java.awt.GridBagConstraints)
+           ~cntr ~container]
+       ~@(loop [result '() body body]
+           (if (empty? body)
+             (reverse result)
+             (let [expr (first body)]
+               (if (keyword? expr)
+                 (recur (cons `(set-grid! ~c ~expr
+                                          ~(second body))
+                              result)
+                        (next (next body)))
+                 (recur (cons `(.add ~cntr ~expr ~c)
+                              result)
+                        (next body)))))))))
+
+(defn flipper-app2 []
+  ;; Construct components:
   (let [flipper (new-flipper)
         b-start (JButton. "Start")
         b-stop (doto (JButton. "Stop")
@@ -52,6 +79,8 @@
         heads (text-field "0")
         t-error (text-field "0.0")
         timer (Timer. 100 nil)]
+
+    ;; Setup actions:
     (with-action timer e
       (let [state @flipper]
         (.setText total (str (:total state)))
@@ -67,17 +96,28 @@
       (.setEnabled b-stop false)
       (.setEnabled b-start true)
       (.stop timer))
+
+    ;; Create window and layout:
     (doto (JFrame. "Flipper")
       (.setContentPane
-       (doto (JPanel.)
-         (.add (JLabel. "Total:"))
-         (.add total)
-         (.add (JLabel. "Heads:"))
-         (.add heads)
-         (.add (JLabel. "Error:"))
-         (.add t-error)
-         (.add b-start)
-         (.add b-stop)))
+       (doto (JPanel. (GridBagLayout.))
+         (grid-bag-layout
+          :insets (Insets. 5 5 5 5)
+
+          :gridx 0, :anchor :LINE_END
+          :gridy 0, (JLabel. "Total:")
+          :gridy 1, (JLabel. "Heads:")
+          :gridy 2, (JLabel. "Error:")
+
+          :gridx 1, :anchor :LINE_START
+          :gridy 0, total
+          :gridy 1, heads
+          :gridy 2, t-error
+
+          :gridx 0, :gridy 3, :gridwidth 2, :anchor :CENTER
+          (doto (JPanel.)
+            (.add b-start)
+            (.add b-stop)))))
       (.pack)
       (.setVisible true)
       (.setDefaultCloseOperation JFrame/DISPOSE_ON_CLOSE))))
@@ -85,4 +125,4 @@
 (defn -main
   "Going through Agents of Swing tutorial. https://stuartsierra.com/2010/01/08/agents-of-swing"
   [& args]
-  (SwingUtilities/invokeLater flipper-app))
+  (SwingUtilities/invokeLater flipper-app2))
