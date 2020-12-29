2e59cd5990e3f4a9faef036771047e8bcdbdf92d
henry brooks <brook057@cougars.csusm.edu>
2020-12-28 20:26:10 -0800

    init

---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject agents-of-swing "0.1.0-SNAPSHOT"
+  :description "Working through Agents of Swing tutorial. https://stuartsierra.com/2010/01/08/agents-of-swing"
+  :url "http://example.com/FIXME"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :main ^:skip-aot agents-of-swing.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all
+                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
